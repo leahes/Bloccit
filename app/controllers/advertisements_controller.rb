a86362f1f4b8a51927de00a7ac8d79a9cb71ead2
Leah <leahstuckey@me.com>
2016-05-13 14:56:40 -0500

    Checkpoint 32 assignment commits - Some might be on Crud32Checkpoint. When I came back to the assignment I didnt realize I was on Master?

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,17 @@
+class AdvertisementsController < ApplicationController
+  def index
+    @advertisements = Advertisement.all
+  end
+
+  def show
+    @advertisement = Advertisement.find(params[:id])
+  end
+
+  def new
+    @advertisement = Advertisement.new
+  end
+
+  def create
+    @advertisement = Advertisement.new
+  end
+end
---
+++ spec/controllers/advertisements_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe AdvertisementsController, type: :controller do
+  let!(:advertisement) do
+    Advertisement.create(
+      title: 'Test',
+      body: 'This is a test',
+      price: 10
+    )
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns the advertisements instance variable" do
+      get :index
+      expect(assigns(:advertisements)).to eq(Advertisement.all)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should set the advertisment instance variable" do
+      get :show, id: advertisement.id
+      expect(assigns(:advertisement)).to eq(advertisement)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @advertisement" do
+      get :new
+      expect(assigns(:advertisement)).not_to be_nil
+    end
+  end
+end
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+
+    it "increases new advertisement by 1" do
+      expect{post :create, advertisement:{title: "Title goes here", body: "Body goes here", price: 10}}.to change {Advertisement.count}.from(0).to(1)
+    end
+
+    it "assignsthe new advertisement to @advertisement" do
+      post :create, advertisement: {title: "Title goes here", body:"Body goes here", price:10}
+      expect(assigns(:advertisement)).to eq Advertisement.old
+    end
+  end
