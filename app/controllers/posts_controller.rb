5ef61b64f1fafcee145d18d7c9db1e265a0f6f56
Leah <leahstuckey@me.com>
2016-05-25 13:39:11 -0500

    Checkpoint-Work-34

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,25 @@
 class PostsController < ApplicationController
-  def index
-    @post = Post.all
-    @post.each_with_index do |post, index|
-    if index % 5 == 0
-      post.title = "SPAM"
-    end
-  end
+
 
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
   def create
     @post = Post.new
     @post.title = params[:post][:title]
-    @post.body = params [:post][:body]
-
-    if @post.Save
+    @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
+    if @post.save
 
       flash[:notice] = "Post was saved successfully"
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
 
       flash.now[:alert] = "There was an error saving the post. Please try again."
@@ -42,7 +38,7 @@ class PostsController < ApplicationController
 
      if @post.save
        flash[:notice] = "Post was updated successfully."
-       redirect_to @post
+       redirect_to [@post.topic,@post]
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :edit
@@ -54,7 +50,7 @@ class PostsController < ApplicationController
 
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post."
       render :show
