694527cb2225cd888c613b4862470f43591342d7
Leah <leahstuckey@me.com>
2016-06-03 10:11:16 -0500

    WIP 1 Assignment-37

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :topics do
   resources :posts, except: [:index]
-end
 
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
 
