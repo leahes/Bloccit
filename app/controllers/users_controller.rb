b8a035d54a8341cc9dd9d9f4d53561bd36420eac
Leah <leahstuckey@me.com>
2016-06-06 17:19:40 -0500

    WIP 37

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,14 @@
 class UsersController < ApplicationController
   def new
-       @user = User.new
-     end
+    @user = User.new
+  end
 
-     def create
-
+  def create
      @user = User.new
      @user.name = params[:user][:name]
      @user.email = params[:user][:email]
      @user.password = params[:user][:password]
      @user.password_confirmation = params[:user][:password_confirmation]
-
 
      if @user.save
        flash[:notice] = "Welcome to Bloccit #{@user.name}!"
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,27 +2,27 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   let(:new_user_attributes) do
-       {
-           name: "BlocHead",
-           email: "blochead@bloc.io",
-           password: "blochead",
-           password_confirmation: "blochead"
-       }
-     end
+     {
+         name: "BlocHead",
+         email: "blochead@bloc.io",
+         password: "blochead",
+         password_confirmation: "blochead"
+     }
+   end
 
-    describe "GET new" do
-      it "returns http success" do
-         get :new
-         expect(response).to have_http_status(:success)
-      end
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
 
-      it "instantiates a new user" do
-          get :new
-          expect(assigns(:user)).to_not be_nil
-         end
-       end
+    it "instantiates a new user" do
+      get :new
+      expect(assigns(:user)).to_not be_nil
+    end
+  end
 
-       describe "POST create" do
+  describe "POST create" do
      it "returns an http redirect" do
        post :create, user: new_user_attributes
        expect(response).to have_http_status(:redirect)
