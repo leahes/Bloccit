ac337904ef5511b7fafd7556a55c4d62839c1d11
Leah <leahstuckey@me.com>
2016-06-16 18:02:33 -0500

    WIP assignment-38 6/16

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+
+    def avatar_url(user)
+      gravatar_id = Digest::MDS::hexidigest(user.email).downcase "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    end
+  end
 end
