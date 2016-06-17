3952c366b606c01ec0016e6b7040541905852615
Leah <leahstuckey@me.com>
2016-06-17 09:02:48 -0500

    Finalcommits Checkpoint-39

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,10 @@
 class TopicsController < ApplicationController
+
+  before_action :require_sign_in, except: [:index, :show]
+
+  before_action :authorize_user, except: [:index, :show]
+
+
   def index
     @topics = Topic.all
   end
@@ -38,6 +44,7 @@ class TopicsController < ApplicationController
      if @topic.save
         flash[:notice] = "Topic was updated successfully."
        redirect_to @topic
+       
      else
        flash.now[:alert] = "Error saving topic. Please try again."
        render :edit
@@ -61,4 +68,11 @@ class TopicsController < ApplicationController
    def topic_params
      params.require(:topic).permit(:name, :description, :public)
    end
-  end
+
+   def authorize_user
+     unless current_user.admin?
+       flash[:alert] = "You must be an admin to do that."
+       redirect_to topics_path
+     end
+   end
+end
