ff14c12a4ecd29dc0bc83b1dedc8bcebb07620ca
Leah <leahstuckey@me.com>
2016-06-17 15:54:57 -0500

    wip 6/17 work-40-41

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
   include SessionsHelper
 
@@ -10,7 +9,7 @@ class ApplicationController < ActionController::Base
     unless current_user
       flash[:alert] = "You must be logged in to do that"
 
-      redirect_to new_session_path
+      redirect_to new_post_session_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :posts, except: [:index]
 end
 
+  resources :posts, only: [] do
+  resources :comments, only: [:create, :destroy]
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
@@ -10,4 +12,5 @@ end
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+  end
 end
