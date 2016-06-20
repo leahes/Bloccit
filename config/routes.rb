024030544ce7e76cec0b82ff223f489e77e06502
Leah <leahstuckey@me.com>
2016-06-20 18:49:29 -0500

    finished checkpoint 40.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
-    resources :users, only: [:new, :create]
   end
 
-    resources :sessions, only: [:new, :create, :destroy]
-    get 'about' => 'welcome#about'
-    root 'welcome#index'
-  end
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'about' => 'welcome#about'
+  root 'welcome#index'
+end
