b4f7eff2413e99656c1ed1abaf994e095df00342
Leah <leahstuckey@me.com>
2016-06-20 09:00:45 -0500

    FINAL commits Assignment-38

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,10 @@ module ApplicationHelper
     else
       content_tag :div, capture(&block), class: 'form-group'
     end
-
-    def avatar_url(user)
-      gravatar_id = Digest::MDS::hexidigest(user.email).downcase "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
-    end
   end
+
+  def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
 end
