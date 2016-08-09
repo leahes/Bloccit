712e260270aaced1788a66390b1153c4f44614a9
Leah <leahstuckey@me.com>
2016-08-09 16:21:07 -0500

    wip

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
+  belongs_to :commentable, polymorphic: true
   belongs_to :user
+  belongs_to :topics
 
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
