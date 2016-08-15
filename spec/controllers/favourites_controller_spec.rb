03f6d09e6e624c72c0e0c5e57d6b2df6a03ac27d
Leah <leahstuckey@me.com>
2016-08-15 18:04:10 -0500

    wip

--- spec/controllers/favourites_controller_spec.rb
+++ spec/controllers/favourites_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include SessionsHelper
 
 RSpec.describe FavouritesController, type: :controller do
-  let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+   let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
    let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
    let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
 
