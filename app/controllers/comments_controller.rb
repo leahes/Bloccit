0f956806c2fd1d05b8cc7c34f863b56f5afe12ea
Leah <leahstuckey@me.com>
2016-08-17 17:26:53 -0500

    wip 42 817

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,21 +8,14 @@ class CommentsController < ApplicationController
       @parent = Post.find id
     elsif params[:topic_id]
       @parent = Topic.find id
-    end
 
     @comment = @parent.comments.find params[:id]
+    @comment.save
+   end
 
-    if comment.save
-      flash[:notice] = 'Comment saved successfully.'
-
-      redirect_to [@post.topic, @post]
-    else
-      flash[:alert] = 'Comment failed to save.'
-
-      redirect_to [@post.topic, @post]
-    end
+   redirect_to [@post.topic, @post]
+   end
   end
-end
 
   def destroy
     @parent  = params[:post_id] || params[:topic_id]
