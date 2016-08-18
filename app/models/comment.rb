1afd77cfd558f42d4158d6133eab7994bc4dd060
Leah <leahstuckey@me.com>
2016-08-17 20:20:32 -0500

    wip 44 817

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,18 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   belongs_to :topics
+  belongs_to :post
 
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
+
+  after_create :send_favourite_emails
+
+  private
+
+  def send_favourite_emails
+     post.favourites.each do |favourite|
+       FavouriteMailer.new_comment(favourite.user, post, self).deliver_now
+    end
+  end
 end
