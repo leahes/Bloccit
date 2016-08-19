93be45fac52a712c059203760cd04efd47380080
Leah <leahstuckey@me.com>
2016-08-19 12:28:12 -0500

    Final work45 commits

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   belongs_to :topics
+  belongs_to :post
 
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
