791d8b0942c60fa484b62c90bc1815bb0dcf60a1
Leah <leahstuckey@me.com>
2016-09-26 18:58:30 -0500

    Final Assign49 commits

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::PostsController < Api::V1::BaseController
-
    before_action :authenticate_user, except: [:index, :show]
    before_action :authorize_user, except: [:index, :show]
 
@@ -8,9 +7,48 @@ class Api::V1::PostsController < Api::V1::BaseController
      render json: posts, status: 200
    end
 
+   def create
+     post = Post.new(post_params)
+     topic = Topic.find(params[:topic_id])
+     post.topic = topic
+     post.user = @current_user
+
+     if post.valid?
+       post.save!
+       render json: post, status: 201
+     else
+       render json: {error: "Post is invalid", status: 400}, status: 400
+     end
+   end
+
+   def update
+      post = Post.find(params[:id])
+
+     if post.update_attributes(post_params)
+       render json: post, status: 200
+     else
+       render json: {error: "Post update failed", status: 400}, status: 400
+     end
+   end
+
+   def destroy
+      post = Post.find(params[:id])
+
+     if post.destroy
+       render json: {message: "Post destroyed", status: 200}, status: 200
+     else
+       render json: {error: "Post destroy failed", status: 400}, status: 400
+     end
+   end
 
    def show
      post = Post.find(params[:id])
      render json: post, status: 200
+   end
+
+   private
+
+   def post_params
+     params.require(:post).permit(:title, :body, :rank)
    end
  end
--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -3,33 +3,67 @@ require 'rails_helper'
  RSpec.describe Api::V1::PostsController, type: :controller do
    let(:my_user) { create(:user) }
    let(:my_post) { create(:post) }
+   let(:my_topic) { my_post.topic }
 
-   context "unauthenticated user" do
-     it "GET index returns http success" do
-       get :index
+  before do
+    my_user.admin!
+    controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+    @new_post = build(:post)
+  end
+
+   describe "POST create" do
+     before { post :create, post: {title: @new_post.title, body: @new_post.body}, topic_id: my_topic.id }
+
+     it "returns http success" do
        expect(response).to have_http_status(:success)
      end
 
-     it "GET show returns http success" do
-       get :show, id: my_post.id
-       expect(response).to have_http_status(:success)
+     it "returns json content type" do
+       expect(response.content_type).to eq 'application/json'
+     end
+
+     it "creates a post with the correct attributes" do
+       hashed_json = JSON.parse(response.body)
+       expect(hashed_json["title"]).to eq(@new_post.title)
+       expect(hashed_json["body"]).to eq(@new_post.body)
      end
    end
 
-   context "unauthorized user" do
-     before do
-       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
-     end
+   describe "DELETE destroy" do
+     before { delete :destroy, id: my_post.id, topic_id: my_topic.id }
 
-
-     it "GET index returns http success" do
-       get :index
+     it "returns http success" do
        expect(response).to have_http_status(:success)
      end
 
-     it "GET show returns http success" do
-       get :show, id: my_post.id
+     it "returns json content type" do
+       expect(response.content_type).to eq 'application/json'
+     end
+
+     it "returns the correct json success message" do
+       expect(response.body).to eq({ message: "Post destroyed", status: 200 }.to_json)
+     end
+
+     it "deletes my_post" do
+       expect{ Post.find(my_post.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+     end
+   end
+
+   describe "PUT update" do
+     before { put :update, id: my_post.id, post: { title: @new_post.title, body: @new_post.body}, topic_id: my_post.id }
+
+     it "returns http success" do
        expect(response).to have_http_status(:success)
+     end
+
+     it "returns json content type" do
+       expect(response.content_type).to eq 'application/json'
+     end
+
+
+     it "updates a post with the correct attributes" do
+       updated_post = Post.find(my_post.id)
+       expect(response.body).to eq(updated_post.to_json)
      end
    end
  end
